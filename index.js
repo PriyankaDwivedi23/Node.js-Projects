028ca2b8b561b89845285c0885902f9354ba8a09
PriyankaDwivedi23 <you@example.com>
2018-04-01 15:45:08 -0400

    Second Deployment

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req,res)=>{
-  res.send("My first deployment on heroku");
+  res.send("Hey, Second commit");
 });
 //port get from heroku
 const PORT = process.env.PORT || 3000;
